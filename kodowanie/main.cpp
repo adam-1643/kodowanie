97a250c46fcff596ba7aad0dfa59817d57a87a2c
Adam Stanisławski <poncki1643@gmail.com>
2018-03-26 15:13:08 +0200

    Initial Commit

---
+++ kodowanie/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  kodowanie
+//
+//  Created by Adam Stanislawski on 26.03.2018.
+//  Copyright © 2018 Adam Stanislawski. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
